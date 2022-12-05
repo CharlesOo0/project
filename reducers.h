c33d795c17f97683101f132232c31c6ae19ad5a4
gabflrt <95493671+gabflrt@users.noreply.github.com>
2022-12-05 15:16:09 +0100

    modif reducers.h

--- reducers.h
+++ reducers.h
@@ -7,14 +7,16 @@
 
 #include "global_defs.h"
 
-typedef struct _recipient {
+typedef struct _recipient
+{
     char recipient_address[STR_MAX_LEN];
     uint32_t occurrences;
     struct _recipient *prev;
     struct _recipient *next;
 } recipient_t;
 
-typedef struct _sender {
+typedef struct _sender
+{
     char recipient_address[STR_MAX_LEN];
     recipient_t *head; // Head of recipient list
     recipient_t *tail; // Tail of recipient list
@@ -25,9 +27,9 @@ typedef struct _sender {
 sender_t *add_source_to_list(sender_t *list, char *source_email);
 void clear_sources_list(sender_t *list);
 sender_t *find_source_in_list(sender_t *list, char *source_email);
-void add_recipient_to_source(sender_t *source, char *recipient_email);
+void add_recipient_to_source(recipient_t *source, char *recipient_email);
 
 void files_list_reducer(char *data_source, char *temp_files, char *output_file);
 void files_reducer(char *temp_file, char *output_file);
 
-#endif //A2022_REDUCERS_H
+#endif // A2022_REDUCERS_H
